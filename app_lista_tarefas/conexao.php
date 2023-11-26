484dd05c4436693e9906cc8f9e6f0970cca50990
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-11-26 09:05:30 -0300

    conexão com o BD

---
+++ app_lista_tarefas/conexao.php
@@ -0,0 +1,24 @@
+<?php
+class Conexao{
+    private $host = 'localhost',
+    private $dbname = 'php_com_pdo',
+    private $user = 'root',
+    private $pass = '',
+
+    public function conectar(){
+        try{
+            $conexao = new PDO(
+                "mysql:host=$this->host;dbname=$this->dbname",
+                "$this->user",
+                "$this->pass"
+            );
+            returno $conexao;
+
+        }catch(PDOException $e){
+            echo '<p>'.$e->getMesseege().'</p>';
+
+        }
+    }
+}
+
+?>
