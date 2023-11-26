e6dc78f26e9a5b40ca9cc2ba7a0fe8dca9051f7a
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-11-26 08:58:37 -0300

    criando classe tarefaservice

---
+++ app_lista_tarefas/tarefas_service.php
@@ -0,0 +1,23 @@
+
+<?php
+class TarefaService{
+
+    public function inserir(){ //create
+
+    }
+
+    public function recuperar(){ //read
+        
+    }
+
+    public function atualizar(){ //update
+        
+    }
+
+    public function remover(){ //delete
+        
+    }
+
+}
+
+?>
