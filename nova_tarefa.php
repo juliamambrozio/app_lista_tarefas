27699f19b5593e22cddc424fa64fe3c8d88c0faa
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2023-11-26 09:14:22 -0300

    criando envio de dados

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -36,10 +36,10 @@
 								<h4>Nova tarefa</h4>
 								<hr />
 
-								<form>
+								<form method="post" action="tarefa_controller.php">
 									<div class="form-group">
 										<label>Descrição da tarefa:</label>
-										<input type="text" class="form-control" placeholder="Exemplo: Lavar o carro">
+										<input type="text" class="form-control" placeholder="Exemplo: Lavar o carro" name="tarefa">
 									</div>
 
 									<button class="btn btn-success">Cadastrar</button>
